b2f1fe18d5360cb4e86ea07d84d90b1cba1891ee
azultasul <azultasul@gmail.com>
2025-04-14 18:16:17 +0900

    합성 증강 중

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from flask import Flask
 import os
+import model.SyntheticAug as SynAug
 import model.Augmentator as Aug
-
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,10 +11,15 @@ def hello_world():
 
 def aug_test():
     print('aug_test!!!')
-    image_dir = './test_data/train/images/'
-    label_dir = './test_data/train/labels/'
-    output_image_dir = './test_data/train/aug_images/'
-    output_label_dir = './test_data/train/aug_labels/'
+    # image_dir = './test_data/train/images/'
+    # label_dir = './test_data/train/labels/'
+    # output_image_dir = './test_data/train/aug_images/'
+    # output_label_dir = './test_data/train/aug_labels/'
+
+    image_dir = './test_data/single_data/train/images/'
+    label_dir = './test_data/single_data/train/labels/'
+    output_image_dir = './test_data/single_data/train/aug_images/'
+    output_label_dir = './test_data/single_data/train/aug_labels/'
 
     os.makedirs(output_image_dir, exist_ok=True)
     os.makedirs(output_label_dir, exist_ok=True)
@@ -24,7 +29,7 @@ def aug_test():
         label_dir=label_dir,
         output_image_dir=output_image_dir,
         output_label_dir=output_label_dir,
-        transform=Aug.transform,
+        transform=SynAug.transform,
         output_num=1, # 한 이미지로 몇장의 증강 데이터를 만들지 결정
     )
     aug.run()
